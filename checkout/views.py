6d24c544f8887eaceda90ada98dcd4dc2de4632f
Tom Cowen <tomreescowen@gmail.com>
2024-08-22 16:45:51 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Pqdn2P1ZrR5cUNYFwT9atwcjZMhh3l1p2f4YHZ1xN7ujRZRUIvNjRzoP7EFUtlTAQi4vVTVQpgL3t6Rwp9UkyPA00zpTSPak5',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
