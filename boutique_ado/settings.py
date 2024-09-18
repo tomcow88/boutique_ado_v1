90243a6fbf2063cdcb8b10095d462243daefb712
Tom Cowen <tomreescowen@gmail.com>
2024-09-18 15:58:01 +0000

    completed confirmation emails

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -175,6 +175,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
